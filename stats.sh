3326a775450708724fcb1c4917031574b6116251
austenwillis <38869666+austenwillis@users.noreply.github.com>
2018-11-19 20:37:57 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -5,12 +5,12 @@ connections=$( netstat | grep tcp )
 users=$( who )
 
 echo -en "\e[33mThis is a snapshot of yor current system:
-
-       \e[32m  Disk Used: $diskused
+       \e[40m
+       \e[32m   Disk Used: $diskused
         
         \e[35m  Free Memory: $memfree
         
-       \e[31m Logged in Users: $users
+       \e[31m  Logged in Users: $users
 \e[46m        
 Open Internet Connections:
 $connections
