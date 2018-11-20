8061e205a79abce63f9277be939fdfca1ca5a7fd
austenwillis <38869666+austenwillis@users.noreply.github.com>
2018-11-19 20:36:51 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -4,7 +4,7 @@ memfree=$(free -mh | grep Mem: | awk ' {print  $7}')
 connections=$( netstat | grep tcp )
 users=$( who )
 
-echo -en "This is a snapshot of yor current system:
+echo -en "\e[33mThis is a snapshot of yor current system:
 
        \e[32m  Disk Used: $diskused
         
